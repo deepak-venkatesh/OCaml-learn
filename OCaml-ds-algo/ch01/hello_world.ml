321d3a6b55106cc59d13c2dea8493a76d577160c
deepak-venkatesh <41579969+deepak-venkatesh@users.noreply.github.com>
2026-01-17 07:23:32 -0800

    ds-algo leap year

--- OCaml-ds-algo/ch01/hello_world.ml
+++ OCaml-ds-algo/ch01/hello_world.ml
@@ -1,1 +1,19 @@
 let () = print_string "Hello World!\n"
+
+(*
+
+In terminal we can compile with
+> ocamlc hello_world.ml
+or
+> ocamlc -o hello hello_world.ml
+
+In the former case the output is a.out and in the latter it is hello
+
+>./a.out
+or
+> ./hello
+
+Output will be printed
+
+
+*)
--- OCaml-ds-algo/ch02/leap_year.ml
+++
@@ -1,1 +0,0 @@
-let year = read_int ()
