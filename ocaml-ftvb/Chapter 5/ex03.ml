133c8c3dce80ba95813ace185aad2ed13e74538f
deepak-venkatesh <41579969+deepak-venkatesh@users.noreply.github.com>
2026-01-19 21:40:01 -0800

    ch5 ex03

---
+++ ocaml-ftvb/Chapter 5/ex03.ml
@@ -0,0 +1,14 @@
+(* insertion sort but with arguments in reverse order *)
+
+let rec insert x lst = 
+  match lst with
+  [] -> [x]
+  | h::t -> if x >= h
+              then x::h::t
+              else h::insert x t
+
+let rec sort lst =
+  match lst with
+  [] -> []
+  | h::t -> insert h (sort t)
+
