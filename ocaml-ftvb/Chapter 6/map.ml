aa86c3667134661a792099ac099510060e0dc77b
deepak-venkatesh <41579969+deepak-venkatesh@users.noreply.github.com>
2026-01-20 21:42:27 -0800

    chapter 6 map

---
+++ ocaml-ftvb/Chapter 6/map.ml
@@ -0,0 +1,11 @@
+(* not sure if there is an inbuilt map function, in common lisp we have mapcar/funcall which is pretty useful *)
+
+let rec map f lst =
+  match lst with
+  [] -> []
+  | h :: t -> f h :: map f t
+
+let halve x = x / 2
+
+
+
